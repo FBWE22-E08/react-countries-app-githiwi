c9cc610d95bc953450ee4e37b2ff22c36d94c97f
hiwi-dci-student <hiwot.abdi@dci.education>
2023-02-20 14:14:41 +0100

    countries displayed using countrycard in allcountries-task2

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import ByRegion from "./components/views/ByRegion";
 import ByName from "./components/views/ByName";
 import Header from "./components/Header";
 import NotFound from "./components/NotFound";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
     <div className="App">
+     
 {/* show the Header component, then add the routes to the pages components*/}
+      <Header/>
+      
+      <Routes>
+        <Route path="/" element={<AllCountries/>}/>
+        <Route path="/ByName" element={<ByName/>}/>
+        <Route path="/ByRegion" element={<ByRegion/>}/>
+        <Route path="*" element={<div> 404 page not found </div>} />
 
-   App component
+      </Routes>
+     
     </div>
   );
 }
--- src/components/views/AllCountries.js
+++ src/components/views/AllCountries.js
@@ -3,10 +3,20 @@ import CountryCard from "../CountryCard";
 
 export default function AllCountries() {
   // fetch all countries on page load (what hook should you use for that?) then save the received array of countries in a local state (call it countries)
+  const [posts, setPost] = useState([]);
 
+  useEffect(() => {
+    fetch(" https://restcountries.com/v2/all")
+      .then((response) => response.json())
+      .then((data) => setPost(data));
+  }, []);
   return (
     <div className="cardsContainer">
+      
       {/* show the countries you received from fetch and saved in the local state (use CountryCard component to display each individual one) */}
+      {posts.map((country)=>(
+        <p>{<CountryCard country={country}/>}</p>
+      ))}
     </div>
   );
 }
